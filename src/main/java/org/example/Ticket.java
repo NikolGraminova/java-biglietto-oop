470338257846222b64bb797cce8242278756bd58
NikolGraminova <nbackup619@gmail.com>
2023-09-04 09:27:25 +0200

    Created project workspace

---
+++ src/main/java/org/example/Ticket.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/*
+Consegna: creare una classe Biglietto con due attributi interi: km e età del passeggero.
+Nel costruttore vanno valorizzati entrambi gli attributi, assicurandosi che abbiano valori validi
+(creare due metodi isValidKm e isValidEta che implementano questa logica).
+In caso anche solo uno non sia valido, sollevare un’eccezione. Aggiungere tre costanti:
+il costo chilometrico di 21 centesimi di €/km (BigDecimal),
+lo sconto over 65 del 40 % (BigDecimal)
+lo sconto minorenni del 20% (BigDecimal).
+Implementare il metodo public che calcola il prezzo del biglietto comprensivo di eventuale sconto (calcolaPrezzo).
+Per eseguire il calcolo dello sconto aggiungere un metodo private calcolaSconto, da chiamare dentro al metodo calcolaPrezzo.
+ */
+
+
+public class Ticket {
+}
